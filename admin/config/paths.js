1ba384eabc6664256b575cb31b07ffa8e128ea1c
Eric Stout <ericwstout@gmail.com>
2018-12-24 20:36:35 -0700

    Compile React view, bailing for custom React implementation

--- admin/config/paths.js
+++ admin/config/paths.js
@@ -69,7 +69,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp(".env"),
   appPath: resolveApp("."),
-  appBuild: resolveApp("../../views/react/"),
+  appBuild: resolveApp("../views/react/"),
   appPublic: resolveApp("public"),
   appHtml: resolveApp("public/index.html"),
   appIndexJs: resolveModule(resolveApp, "src/index"),
